ba9205d83eb4528d1c9b8fb7e5cc615fa4faf497
ppxu <xyang0110@126.com>
2015-12-21 16:24:49 +0800

    auto deploy at 2015/12/21-16:24:49

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "\033[32m deploy hexo start \033[0m"
-echo "\033[32m hexo generate...  \033[0m"
+echo -e "\033[32m deploy hexo start \033[0m"
+echo -e "\033[32m hexo generate...  \033[0m"
 hexo g
-echo "\033[32m git commit...  \033[0m"
+echo -e "\033[32m git commit...  \033[0m"
 d=`date +%x-%T`
 git add .
 git commit -am "auto deploy at "${d}
-echo "\033[32m git push...  \033[0m"
+echo -e "\033[32m git push...  \033[0m"
 git push origin master
-echo "\033[32m deploy hexo finish  \033[0m"
+echo -e "\033[32m deploy hexo finish  \033[0m"
