5f0a5d27a87caf06494e69223c4d3d0a8f46ce3a
ppxu <xyang0110@126.com>
2015-12-21 16:17:51 +0800

    auto deploy at 2015/12/21-16:17:51

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "start deploy hexo..."
+echo "hexo generate..."
+hexo g
+echo "git commit..."
+d=`date +%x-%T`
+git add .
+git commit -am "auto deploy at "${d}
+echo "git push..."
+git pusn origin master
