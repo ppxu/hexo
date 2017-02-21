0a00dae982a93420118242d0ba066045f331ee10
ppxu <xyang0110@126.com>
2017-02-21 15:48:16 +0800

    update

--- sync.sh
+++ sync.sh
@@ -2,6 +2,8 @@
 
 echo -e "\033[32m [AUTO SYNC] sync hexo start \033[0m"
 cd /ppxu/blog
+echo -e "\033[32m [AUTO SYNC] git reset... \033[0m"
+git reset --hard
 echo -e "\033[32m [AUTO SYNC] git pull...  \033[0m"
 git pull origin master
 echo -e "\033[32m [AUTO SYNC] sync hexo finish \033[0m"
